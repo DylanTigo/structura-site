b722c239eb92348fdb788c355a365059df379ff9
DylanTigo <dylannoeltigomo@gmail.com>
2025-03-07 12:53:08 +0100

    Add logo image and update Header and Menu components to include it

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useState } from "react";
 import Menu from "./Menu";
+import Image from "next/image";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -14,7 +15,8 @@ const Header = () => {
   return (
     <>
       <div className="flex items-center h-16 px-3 font-medium font-chakra relative">
-        <Link href="/" className="uppercase">
+        <Link href="/" className="uppercase flex items-center gap-1">
+          <Image src="/logo.png" alt="Logo" width={16} height={16} />
           Structura
         </Link>
         <button type="button" onClick={handleMenuClick} className="absolute right-3 flex items-center gap-1.5 uppercase z-50">
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,31 +1,48 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
 
 type MenuProps = {
-  isOpen: boolean
-  onClose: () => void
-}
-const Menu = ({isOpen, onClose}: MenuProps) => {
-  if (!isOpen) return null
+  isOpen: boolean;
+  onClose: () => void;
+};
+const Menu = ({ isOpen, onClose }: MenuProps) => {
+  if (!isOpen) return null;
   return (
-    <div className='fixed top-0 left-0 w-screen h-screen font-chakra z-40'>
-      <div onClick={onClose} className="absolute top-0 left-0 w-full h-full bg-black/40 z-10 blur-lg"></div>
+    <div className="fixed top-0 left-0 w-screen h-screen font-chakra z-40">
+      <div
+        onClick={onClose}
+        className="absolute top-0 left-0 w-full h-full bg-black/40 z-10 blur-lg"
+      ></div>
       <div className="absolute top-0 right-0 h-full z-20 px-3 pb-5 bg-green flex flex-col justify-between">
-        <nav className="flex flex-col pt-20 w-full sm:w-96 text-5xl z-20"> 
-          <Link href="/" className='p-4 w-full'>Home</Link>
-          <Link href="/" className='p-4 w-full'>About</Link>
-          <Link href="/" className='p-4 w-full'>Projects</Link>
-          <Link href="/" className='p-4 w-full'>Blogs</Link>
-          <Link href="/" className='p-4 w-full'>Contact</Link>
+        <nav className="flex flex-col pt-20 w-full sm:w-96 text-5xl z-20">
+          <Link href="/" className="p-4 w-full">
+            Home
+          </Link>
+          <Link href="/" className="p-4 w-full">
+            About
+          </Link>
+          <Link href="/" className="p-4 w-full">
+            Projects
+          </Link>
+          <Link href="/" className="p-4 w-full">
+            Blogs
+          </Link>
+          <Link href="/" className="p-4 w-full">
+            Contact
+          </Link>
         </nav>
 
         <div className="text-center">
-          <hr className='opacity-50' />
-          <div className="mt-3.5 text-sm uppercase font-medium">Stuctura</div>
+          <hr className="opacity-50" />
+          <div className="flex items-center gap-1 mt-3.5 text-sm uppercase font-medium">
+            <Image src="/logo.png" alt="Logo" width={16} height={16} />
+            Stuctura
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
